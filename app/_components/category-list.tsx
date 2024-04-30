fc147139eefcf80c0d88c9a7b073170b956481e2
fomes <fgomesdeluna@gmail.com>
2024-04-30 15:32:51 -0300

    feat: add category item and category list

---
+++ app/_components/category-list.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { db } from "../_lib/prisma";
+import { CategoryItem } from "./category-item";
+
+export const CategoryList = async () => {
+  const categories = await db.category.findMany({});
+
+  return (
+    <div className="grid grid-cols-2 gap-3">
+      {categories.map((item) => (
+        <CategoryItem key={item.id} category={item} />
+      ))}
+    </div>
+  );
+};
