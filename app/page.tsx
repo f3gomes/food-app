5d32025f96eeb4c0729b96a70c9441cc02954165
fomes <fgomesdeluna@gmail.com>
2024-05-02 12:14:49 -0300

    feat: add restaurant recommended page

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 import { ChevronRightIcon } from "lucide-react";
 
+import Link from "next/link";
+
 import { db } from "./_lib/prisma";
 import { Header } from "./_components/header";
 import { Search } from "./_components/search";
@@ -53,13 +55,15 @@ const Home = async () => {
       <div className="space-y-4 pt-6">
         <div className="flex items-center justify-between px-5">
           <h2 className="font-semibold">Pedidos Recomendados</h2>
-          <Button
-            variant="ghost"
-            className="h-fit p-0 text-primary hover:bg-transparent"
-          >
-            Ver todos
-            <ChevronRightIcon size={16} />
-          </Button>
+          <Link href="/restaurants/recommended">
+            <Button
+              variant="ghost"
+              className="h-fit p-0 text-primary hover:bg-transparent"
+            >
+              Ver todos
+              <ChevronRightIcon size={16} />
+            </Button>
+          </Link>
         </div>
         <ProductList products={products} />
       </div>
