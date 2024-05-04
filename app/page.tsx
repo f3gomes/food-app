cab1440df5790d6d13bcafdcefaa4df6d68b4fc3
fomes <fgomesdeluna@gmail.com>
2024-05-04 15:51:59 -0300

    feat: add initial menu sheet

--- app/page.tsx
+++ app/page.tsx
@@ -3,14 +3,13 @@ import { ChevronRightIcon } from "lucide-react";
 import Link from "next/link";
 
 import { db } from "./_lib/prisma";
-import { Header } from "./_components/header";
-
+import Search from "./_components/search";
+import Header from "./_components/header";
 import { Button } from "./_components/ui/button";
 import ProductList from "./_components/product-list";
 import PromoBanner from "./_components/promo-banner";
 import { CategoryList } from "./_components/category-list";
 import RestaurantList from "./_components/restaurant-list";
-import Search from "./_components/search";
 
 const Home = async () => {
   const products = await db.product.findMany({
